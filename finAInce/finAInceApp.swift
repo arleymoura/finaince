58106637772e1a83eab4d3836753c2c5561020c6
Arley Moura <arleymoura@gmail.com>
2026-04-13 22:07:33 +0200

    Initial Commit

---
+++ finAInce/finAInceApp.swift
@@ -0,0 +1,17 @@
+//
+//  finAInceApp.swift
+//  finAInce
+//
+//  Created by Arley Moura on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct finAInceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
